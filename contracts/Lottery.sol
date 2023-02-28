1c64f00dd7df441a93661edf363ca9433fde88a7
Femi Ajayi <femiajayi705@gmail.com>
2023-02-28 08:07:22 +0100

    feat: Created lottery contract

---
+++ contracts/Lottery.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.4.17;
+
+contract Lottery {
+    address public manager;
+    address[] public players;
+
+    constructor() {
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > .01 ether);
+        players.push(msg.sender);
+    }
+
+    function random() private view returns (uint256) {
+        return uint256(keccak256(now));
+    }
+
+    function pickWinner() public restricted {
+        uint256 index = random() % players.length;
+        players[index].transfer(this.balance);
+        players = new address[](0);
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    function getPlayers() public view returns (address[] memory) {
+        return players;
+    }
+}
